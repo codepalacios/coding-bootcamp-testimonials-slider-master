1d0a558bc7d0ad51ea37b9fae5fd818218cc8e4f
codepalacios <chrispalacios95@gmail.com>
2024-09-11 23:37:36 -0600

    Finalized desktop styles

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -7,8 +7,8 @@ function App() {
   return (
     <main className="app">
       <section className="app__section">
-        <Testimonial className="app__testimonial"/>
-        <TestimonialImage className="app__testimonialImage"/>
+        <Testimonial />
+        <TestimonialImage />
       </section>
     </main>
   );
